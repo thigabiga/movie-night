ad63dc219e2e7738a7fa3b7525a4ab044b7f7a64
Erin Thigpen <35975360+thigabiga@users.noreply.github.com>
2018-05-08 13:31:31 -0500

    added basic routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React, {Component} from "react";
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import FilterableMovieTable from "./Components/FilterableMovieTable.js";
-
-// const MOVIES = [
-//   {id: 0, movieTitle: 'Top Gun', seen: true, infoLink: '#', position: 1},
-//   {id: 1, movieTitle: 'When Harry Met Sally', seen: true, infoLink: '#', position: 2},
-//   {id: 2, movieTitle: 'Notting Hill', seen: true, infoLink: '#', position: 4},
-//   {id: 3, movieTitle: 'Atonement', seen: true, infoLink: '#', position: 5},
-//   {id: 4, movieTitle: 'Fight Club', seen: true, infoLink: '#', position: 6},
-//   {id: 5, movieTitle: 'Pulp Fiction', seen: false, infoLink: '#', position: 3}
-// ];
+import Home from "./Components/Home.js";
+import Browse from "./Components/Browse.js";
+import NotFound from "./Components/NotFound.js";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <FilterableMovieTable />
-      </div>
+      <Router>
+        <div>
+          <p>
+            <Link to="/">Home</Link>
+          </p>
+          <p>
+            <Link to="/browse">Browse</Link>
+          </p>
+          <p>
+            <Link to="/lists">Lists</Link>
+          </p>
+
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/browse" component={Browse} />
+            <Route path="/lists" component={FilterableMovieTable} />
+            <Route path="/*" component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/Components/Browse.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Browse extends Component {
+    render() {
+
+    return (
+        <div>Browse</div>
+    )
+}
+}
+
+export default Browse;
