4ccb42259d5d23b35bd4a4de4fa69a861aa11553
Erin Thigpen <35975360+thigabiga@users.noreply.github.com>
2018-05-08 22:14:37 -0500

    added drawer for filter options

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,12 +1,17 @@
 import React, {Component} from "react";
 import {Link} from 'react-router-dom';
 
+// STYLES
 import {Toolbar, ToolbarGroup, ToolbarTitle} from 'material-ui/Toolbar';
 import TextField from 'material-ui/TextField';
 import Checkbox from 'material-ui/Checkbox';
 import FontIcon from 'material-ui/FontIcon';
 import IconButton from 'material-ui/IconButton';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
 
+// ICONS
 const addIcon = <FontIcon className="material-icons">library_add</FontIcon>;
 const filterIcon = <FontIcon className="material-icons">filter_list</FontIcon>;
 
@@ -16,7 +21,12 @@ class SearchBar extends Component {
     super(props);
     this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
     this.handleSeenOnlyChange = this.handleSeenOnlyChange.bind(this);
+    this.state = {open: false};
   }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  handleClose = () => this.setState({open: false});
 
   handleFilterTextChange(e) {
     this.props.onFilterTextChange(e.target.value);
@@ -40,6 +50,7 @@ class SearchBar extends Component {
             <IconButton
               tooltip="filter list"
               tooltipPosition="bottom-right"
+              onClick={this.handleToggle}
             >{filterIcon}</IconButton>
           </ToolbarGroup>
           <ToolbarGroup>
@@ -53,13 +64,29 @@ class SearchBar extends Component {
             />
           </ToolbarGroup>
         </Toolbar>
-        <form>
-          <Checkbox
-            label="Seen Only."
-            checked={this.props.seenOnly} 
-            onCheck = {this.handleSeenOnlyChange}
-          />
-        </form>
+
+        <Drawer
+          docked={false}
+          width={200}
+          open={this.state.open}
+          onRequestChange={(open) => this.setState({open})}
+        >
+          <MenuItem>Filter</MenuItem>
+          <MenuItem>          
+            <Checkbox
+              label="Erin's Seen"
+              checked={this.props.seenOnly} 
+              onCheck = {this.handleSeenOnlyChange}
+            />
+          </MenuItem>
+          <MenuItem>
+            <RaisedButton
+              label="Done"
+              onClick={this.handleClose}
+            />
+          </MenuItem>
+        </Drawer>
+
       </div>
     );
   }
